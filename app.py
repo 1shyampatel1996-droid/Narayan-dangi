068e7c22a064b50d42970e9ed57131ea54349181
Narayan dangi <1shyampatel1996@gmail.com>
2026-08-14 17:04:23 +0530

    Refactor HTML table to Streamlit markdown format

--- app.py
+++ app.py
@@ -21,35 +21,34 @@ sensex_fut_open, sensex_fut_fin = "78278.0", "325"
 n_col1, n_col2 = get_color(nifty_idx_open, nifty_idx_fin)
 s_col1, s_col2 = get_color(sensex_idx_open, sensex_idx_fin)
 
-# HTML टेबल फॉर्मेट ताकि मोबाइल पर भी एक ही लाइन में रहे
-html_code = f"""
-<div style="font-size: 16px; line-height: 2.2;">
-    <b>1. Nifty Group</b><br>
-    <div style="display: flex; justify-content: space-between; padding: 2px 0;">
-        <span style="width: 40%;"><b>Nifty 50</b></span>
-        <span style="width: 35%; text-align: right;">{nifty_idx_open}</span>
-        <span style="width: 20%; text-align: right; color: {n_col2}; font-weight: bold;">{nifty_idx_fin}</span>
-    </div>
-    <div style="display: flex; justify-content: space-between; padding: 2px 0;">
-        <span style="width: 40%;"><b>Future</b></span>
-        <span style="width: 35%; text-align: right;">{nifty_fut_open}</span>
-        <span style="width: 20%; text-align: right; color: {n_col1}; font-weight: bold;">{nifty_fut_fin}</span>
-    </div>
-    
-    <hr style="margin: 10px 0;">
-    
-    <b>2. Sensex Group</b><br>
-    <div style="display: flex; justify-content: space-between; padding: 2px 0;">
-        <span style="width: 40%;"><b>Sensex</b></span>
-        <span style="width: 35%; text-align: right;">{sensex_idx_open}</span>
-        <span style="width: 20%; text-align: right; color: {s_col2}; font-weight: bold;">{sensex_idx_fin}</span>
-    </div>
-    <div style="display: flex; justify-content: space-between; padding: 2px 0;">
-        <span style="width: 40%;"><b>Future</b></span>
-        <span style="width: 35%; text-align: right;">{sensex_fut_open}</span>
-        <span style="width: 20%; text-align: right; color: {s_col1}; font-weight: bold;">{sensex_fut_fin}</span>
-    </div>
+# 1. Nifty Group
+st.markdown("### 1. Nifty Group")
+st.markdown(f"""
+<div style="display: flex; justify-content: space-between; padding: 4px 0; font-size: 16px;">
+    <span style="width: 40%;"><b>Nifty 50</b></span>
+    <span style="width: 35%; text-align: right;">{nifty_idx_open}</span>
+    <span style="width: 20%; text-align: right; color: {n_col2}; font-weight: bold;">{nifty_idx_fin}</span>
 </div>
-"""
+<div style="display: flex; justify-content: space-between; padding: 4px 0; font-size: 16px;">
+    <span style="width: 40%;"><b>Future</b></span>
+    <span style="width: 35%; text-align: right;">{nifty_fut_open}</span>
+    <span style="width: 20%; text-align: right; color: {n_col1}; font-weight: bold;">{nifty_fut_fin}</span>
+</div>
+""", unsafe_allow_html=True)
 
-st.markdown(html_code, unsafe_allow_html=True)
+st.markdown("---")
+
+# 2. Sensex Group
+st.markdown("### 2. Sensex Group")
+st.markdown(f"""
+<div style="display: flex; justify-content: space-between; padding: 4px 0; font-size: 16px;">
+    <span style="width: 40%;"><b>Sensex</b></span>
+    <span style="width: 35%; text-align: right;">{sensex_idx_open}</span>
+    <span style="width: 20%; text-align: right; color: {s_col2}; font-weight: bold;">{sensex_idx_fin}</span>
+</div>
+<div style="display: flex; justify-content: space-between; padding: 4px 0; font-size: 16px;">
+    <span style="width: 40%;"><b>Future</b></span>
+    <span style="width: 35%; text-align: right;">{sensex_fut_open}</span>
+    <span style="width: 20%; text-align: right; color: {s_col1}; font-weight: bold;">{sensex_fut_fin}</span>
+</div>
+""", unsafe_allow_html=True)
