3d23662f4aec2eec1f72c8e3fff3929404cae10f
Narayan dangi <1shyampatel1996@gmail.com>
2026-08-14 18:41:13 +0530

    Fix import statement for SmartConnect

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pyotp
 import streamlit as st
-from smartapi.smartConnect import SmartConnect
-
+from SmartApi.smartConnect import SmartConnect
 
 # पेज सेटअप
 st.set_page_config(page_title="Market Live App", layout="wide")
